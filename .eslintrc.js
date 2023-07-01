2c8fe2de023bf3c55af95df11ea4d4eb91d55674
中山一輝 <nakayama@mackbook-air.local>
2023-07-01 11:24:28 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:prettier/recommended", 'plugin:react/recommended', "plugin:react-hooks/recommended"],
+  plugins: ["@typescript-eslint", 'prettier'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  ignorePatterns: [
+    "**/*.js",
+  ],
+  settings: {
+    react: {
+        version: 'detect',
+    },
+  },
+  rules: {
+    "no-console": "warn",
+    "@typescript-eslint/adjacent-overload-signatures": "warn",
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
+    "react/display-name": "off",
+    "react/prop-types": "off",
+    "react-hooks/exhaustive-deps": "warn"
+  }
+}
